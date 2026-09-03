54715b57cdc94d9d2ca30ba3eff23ac65ab82722
alejandrahernandez24a <alejandra.hernandez24a@udgvirtual.udg.mx>
2026-09-03 13:45:40 -0600

    Agrega botones de navegacion y mejora interfaz

---
+++ Assets/Scenes/BotonesNavegacion.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class BotonesNavegacion : MonoBehaviour
+{
+    public void Inicio()
+    {
+        Debug.Log("Estas en inicio");
+    }
+
+    public void Siguiente()
+    {
+        Debug.Log("Apretaste el boton siguiente");
+    }
+
+    public void Anterior()
+    {
+        Debug.Log("Apretaste el boton anterior");
+    }
+}
